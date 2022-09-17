8726458bad9b66c5083694aaacc0439a7a13e90a
LENOVO IDEAPAD <lazaronoel.g@gmail.com>
2022-09-17 00:46:32 -0400

    Default Project

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Routes, RouterModule} from '@angular/router'
+
+const routes: Routes = [
+]
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports:[RouterModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
